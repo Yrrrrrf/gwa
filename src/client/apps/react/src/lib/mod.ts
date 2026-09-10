0fc03a062ad54f5c3823133163f24ca7f27e70d8
Fernando Bryan Reza Campos <fer.rezac@outlook.com>
2026-09-09 18:40:28 -0600

    Remove boierlplate

--- src/client/apps/react/src/lib/mod.ts
+++ src/client/apps/react/src/lib/mod.ts
@@ -1,19 +1,5 @@
-import { Counter as _Counter, Icon as _Icon } from "@sdk/ui";
-import type { FC } from "react";
 import faviconUrl from "./assets/img/react.svg";
 
-export const Counter = _Counter as unknown as FC<{
-	initial?: number;
-	count?: number;
-	onchange?: (count: number) => void;
-}>;
-
-export const Icon = _Icon as unknown as FC<{
-	route: string;
-	size?: number;
-	color?: string;
-	class?: string;
-}>;
-
+export { Counter, Icon } from "@sdk/ui";
 export { faviconUrl };
 export default faviconUrl;
